3b0bb0161ae0aa34be6aa2a565745a4702f87eec
papiasharmin <papiasharmin33@gmail.com>
2023-11-09 22:14:42 +0900

    updated ui/ux, login, multisignature wallet

--- thirdapp/src/components/App.jsx
+++ thirdapp/src/components/App.jsx
@@ -4,26 +4,16 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './../assets/css/App.css';
 import { useMyContext } from './../Contexts';
-
-import {
-  connectWallet
-} from './hooks/UseContract';
 import Buy from './pages/Buy';
 import Create from './pages/Create';
 import Home from './pages/Home';
 import NoPage from './pages/NoPage';
-import Nfts from "./pages/Nfts";
 import Txs from './pages/Tx/Txs';
 import Upload from './pages/Upload';
 import MyVC from './pages/Vc/MyVc';
 import Verify from './pages/Verify';
 import Wallets from './pages/Wallet/Wallets';
-import Example from './pages/SwapToken';
 import Login from "./pages/Login";
-import { ethers } from 'ethers';
-import logo from '../assets/imgs/logo.png'
-import superAgent from 'superagent';
-import { baseURL } from './common/Constant';
 import { ConnectWallet,useAddress, useConnectionStatus } from "@thirdweb-dev/react";
 import './../stylesheets/header.css'
 import './../stylesheets/login.css'
@@ -51,87 +41,23 @@ function App() {
 
 const address = useAddress();
 
-//const connectthird = useConnect();
-
-// async function handleConnect() {
-//   try {
-//     const wallet = await connect(
-//       walletConfig, // pass the wallet config object
-//        // pass options required by the wallet (if any)
-//     );
-
-//     console.log("connected to", wallet);
-//   } catch (e) {
-//     console.error("failed to connect", e);
-//   }
-// }
 
 const [anchorEl, setAnchorEl] = useState(null);
-// メニュー用の変数
+
 const open = Boolean(anchorEl);
 
-/**
- * メニューアイコンをクリックした時の処理
- * @param {*} event イベントハンドラ
- */
 const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
 };
 
-/**
- * メニューバーを閉じる時の処理
- */
 const handleClose = () => {
       setAnchorEl(null);
 };
 
-  // const [data, setdata] = useState({
-  //   address: "",
-  //   Balance: null,
-  // });
-
-
-  // const [pass,setpass] = useState(null)
-  // const [conpass,setconpass] = useState(null)
    const [isLoading, setIsLoading] = useState(false);
    const [web3auth, setweb3auth] = useState(null)
-  // const [usercreated, setusercreated] = useState(null)
-  // const [qrOpen, setQrOpen] = useState(false);
 
   const activeChain = "ethereum";
-  /**
-   * ウォレット接続ボタンを押した時の処理
-   */
-
-  function createuser(userdata){
-      setIsLoading(true);
-   
-      // 送金用のAPIを呼び出す
-      superAgent
-          .post(baseURL + '/api/createuser')
-          .query({
-              data: userdata,   
-          })
-          .end(async(err, res) => {
-              if (err) {
-                  console.log("Token送金用API呼び出し中に失敗", err);
-                  // popUpメソッドの呼び出し
-                 // popUp(false, "failfull...");
-                  setIsLoading(false);
-                  return err;
-              }
-              console.log('RESPONSEBODY',res.body)
-              setIsLoading(false);
-              setCurrentAccount(res.body.addr)
-              // popUpメソッドの呼び出し
-              //popUp(true, "successfull!!");
-          });
-  }
-
-
-
-
-
 
 const copy = () => {
   //コピー
@@ -163,8 +89,6 @@ const copy = () => {
               <Route path="/Myvc" exact element={ <MyVC /> } />
               <Route path="/Upload" exact element={ <Upload /> } />
               <Route path="/Verify" exact element={ <Verify/> } />
-              
-              <Route path="/Swaptoken" exact element={ <Example/> } />
               <Route path="*" exact element={ <NoPage/> } />
             </Routes>
            
@@ -179,4 +103,3 @@ const copy = () => {
 
 export default App;
 
-//<Route path="/Nfts" exact element={ <Nfts/> } />
--- thirdapp/src/components/hooks/UseContract.js
+++ thirdapp/src/components/hooks/UseContract.js
@@ -10,18 +10,13 @@ import { CHAIN_ID, CONTRACT_ADDRESS, MYTOKEN_ADDRESS, RPC_URL } from "./../commo
 import { ethers } from 'ethers';
 
 
-/**
- * createContractObject メソッド
- * @param contractAbi コントラクトABI
- * @param contractAddress コントラクトアドレス
- */
 export const createWllet= () =>{
 
       let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
       return provider;
 }
 export const mainnetProvider = new ethers.providers.JsonRpcProvider(
-      `https://polygon-mainnet.g.alchemy.com/v2/iZZGP25xnifffDm7m4A3BwXHTTdNufws`
+      `https://polygon-mainnet.g.alchemy.com/v2/${process.env.APIKEY}`
     )
 export const getProvider= () =>{
       let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
@@ -32,113 +27,14 @@ console.log('CHAIN',CHAIN_ID,RPC_URL)
 const createContractObject = ( contractAbi, contractAddress) => {
       // get provider
 
-      let provider = new ethers.providers.JsonRpcProvider(RPC_URL);//new ethers.JsonRpcProvider(RPC_URL);
+      let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
       let contract = new ethers.Contract(contractAddress, contractAbi, provider)
       
       return contract
 };
 
-// import the builder util
-
-
-// web3wallet.on('session_proposal', async sessionProposal => {
-//   const { id, params } = sessionProposal
-
-//   // ------- namespaces builder util ------------ //
-//   const approvedNamespaces = buildApprovedNamespaces({
-//     proposal: params,
-//     supportedNamespaces: {
-//       eip155: {
-//         chains: ['eip155:80001'],
-//         methods: ['eth_sendTransaction', 'personal_sign'],
-//         events: ['accountsChanged', 'chainChanged'],
-//         accounts: [
-//           'eip155:80001:0x202E34b639EEE7377aB5d80606f933b8c9c7Bae6',
-         
-//         ]
-//       }
-//     }
-//   })
-//   // ------- end namespaces builder util ------------ //
-
-//   const session = await web3wallet.approveSession({
-//     id,
-//     namespaces: approvedNamespaces
-//   })
-// })
-
-
-//     const {topic, uri} = await AuthClient.core.pairing.create()
-//     await web3wallet.pair({ uri })
-
-/**
- * connectWallet メソッド
- */
- export const connectWallet = async() => {
-      // create bloctoSDK object
-      // const bloctoSDK = new BloctoSDK({
-      //       ethereum: {
-      //           chainId: CHAIN_ID, 
-      //           rpc: RPC_URL,
-      //       }
-      // });
-      // // request
-      // const signers = await bloctoSDK.ethereum.request({ method: 'eth_requestAccounts' });
-      
-      // const signer = signers[0]
-      
-      // console.log('BLOCKTO',bloctoSDK.ethereum, signers)
-      
-// console.log("process.env.CONNECT_KEY", process.env.CONNECT_KEY)
-// const authClient = await AuthClient.init({
-//   projectId: "6df1086ec4c69b1400bf2426405b9a93",
-//   metadata: {
-//     name: 'idq-soul-wallet',
-//     description: 'A wallet using WalletConnect AuthClient',
-//     url: 'https://idq-front.vercel.app',
-//     icons: ['']
-//   }
-// })
-//       //console.log(authClient.emit("auth_request"))
-//       authClient.emit("auth_request")
-//       authClient.on('auth_request', async ({ id, params }) => {
-//             // the user’s address
-//             let wallet = ethers.Wallet.createRandom()
-//             const iss = `did:pkh:eip155:80001:${wallet.address}`
-          
-//             // format the cacao payload with the user’s address
-//             const message = authClient.formatMessage(params.cacaoPayload, iss)
-          
-//             // This is a good point to trigger a UI event to provide the user
-//             // with a button to accept or reject the authentication request,
-//             // instead of automatically responding.
-//             const signature = await wallet.signMessage(message)
-          
-//             await authClient.respond(
-//               {
-//                 id: id,
-//                 signature: {
-//                   s: signature,
-//                   t: 'eip191'
-//                 }
-//               },
-//               iss
-//             )
-          
-//           })
-//           const {topic, uri} = await authClient.core.pairing.create()
-//           await authClient.core.pairing.pair({ uri })
-
-          
-      
- };
-
-/**
- * getDidメソッド
- * @param signer ログイン中のsignerオブジェクト
- */
 export const getDid = async(signer) => {
-      // call createContractObject メソッド
+    
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
        
        const result = await FactoryContract.dids(signer) 
@@ -146,13 +42,10 @@ export const getDid = async(signer) => {
       return result;
 };
 
-/**
- * getTokenBalanceOf メソッド
- * @param signer ログイン中のsignerオブジェクト
- */
+
 export const getTokenBalanceOf = async(signer) => {
       
-      // call createContractObject メソッド
+ 
       const MyTokenContract  = createContractObject(MyToken.abi, MYTOKEN_ADDRESS);
       // get token balance
       const num = await MyTokenContract.balanceOf(signer)
@@ -160,12 +53,8 @@ export const getTokenBalanceOf = async(signer) => {
       return Number(num);
 };
 
-/**
- * getRegisterStatusメソッド
- * @param signer ログイン中のsignerオブジェクト
- */
 export const getRegisterStatus = async(signer) => {
-      // call createContractObject メソッド
+
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
       // get status info
       var status = await FactoryContract.isRegistered(signer)
@@ -173,7 +62,7 @@ export const getRegisterStatus = async(signer) => {
 };
 
 export const userexist = async(email) => {
-      // call createContractObject メソッド
+      
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
       // get status info
       const status = await FactoryContract.userExist(email)
@@ -185,12 +74,9 @@ export const userexist = async(email) => {
 };
 
 
-/**
- * getVcsメソッド
- * @param did ログイン中のdid情報
- */
+
 export const getVcs = async(did) => {
-      // call createContractObject メソッド
+      
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
       // get Verifiable Credentials info
       var vcs = await FactoryContract.getVcs(did);
@@ -198,11 +84,9 @@ export const getVcs = async(did) => {
       return vcs;
 };
 
-/**
- * walletsCountメソッド
- */
+
 export const walletsCount = async() => {
-      // call createContractObject メソッド
+     
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
       // get wallet addresses
       const count = await FactoryContract.walletsCount();
@@ -211,13 +95,9 @@ export const walletsCount = async() => {
 };
 
 
-/**
- * getWalletsメソッド
- * @param count count 
- * @param start start
- */
+
 export const getWallets = async(count, start) => {
-      // call createContractObject メソッド
+    
       const FactoryContract = createContractObject(WalletFactory.abi, CONTRACT_ADDRESS);
       // get Verifiable Credentials info
       const multiSigWallets = await FactoryContract.getWallets(count, start);
@@ -227,14 +107,11 @@ export const getWallets = async(count, start) => {
 
 
 
-/**
- * getWalletInfoメソッド
- * @param addr ウォレットアドレス
- */
+
 export const getWalletInfo = async(addr) => {
-      // call createContractObject メソッド
+
       const WalletContract = createContractObject(MultiSigWallet.abi, addr);
-      //console.log('',WalletContract.methods)
+      
       // get Verifiable Credentials info
        const wName = await WalletContract.getName();
        let required = await WalletContract.getRequired();
@@ -254,41 +131,31 @@ export const getWalletInfo = async(addr) => {
       };
 };
 
-/**
- * getApprovalCount メソッド
- * @param addr ウォレットアドレス
- * @param index ウォレットID
- */
+
 export const getApprovalCount = async(addr, index) => {
-      // call createContractObject メソッド
+
       const WalletContract = createContractObject(MultiSigWallet.abi, addr);
-      // 承認済みの数を求める
+     
       const approvement = await WalletContract._getApprovalCount(index);
        console.log('aprove',approvement)
       return Number(approvement);
 }; 
 
-/**
- * getRequired メソッド
- * @param addr ウォレットアドレス
- */
+
 export const getRequired = async(addr) => {
-      // call createContractObject メソッド
+ 
       const WalletContract = createContractObject(MultiSigWallet.abi, addr);
-      // 閾値を取得する。
+    
       const req = await WalletContract.getRequired();
       console.log('require',req)
       return Number(req);
 }; 
 
-/**
- * getTxs メソッド
- * @param addr ウォレットアドレス
- */
+
 export const getTxs = async(addr) => {
-      // call createContractObject メソッド
+      
       const WalletContract = createContractObject(MultiSigWallet.abi, addr);
-      // トランザクションの情報を取得する。
+      
       const transactions = await WalletContract.getTxs();
       console.log('transactions',transactions)
       return transactions;
